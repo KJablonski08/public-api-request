3315d375f89822cae16b384c42310e619278d379
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-08 22:29:02 -0400

    rendering card for one employee

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@
 let employees = [];
 let employeesContainer = document.getElementById('gallery')
 
-function getEmployees() {
+function getEmployee() {
    baseUrl = 'https://randomuser.me/api/'
 
    fetch(baseUrl)
@@ -21,18 +21,19 @@ function getEmployees() {
 }
    
 function renderEmployee(data) {
+   console.log(data)
    const html = 
       `<div class="card">
       <div class="card-img-container">
-         <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+         <img class="card-img" src="${data[0].picture.large}" alt="profile picture">
       </div>
       <div class="card-info-container">
-         <h3 id="name" class="card-name cap">first last</h3>
-         <p class="card-text">email</p>
-         <p class="card-text cap">city, state</p>
+         <h3 id="name" class="card-name cap">${data[0].name.first} ${data[0].name.last}</h3>
+         <p class="card-text">${data[0].email}</p>
+         <p class="card-text cap">${data[0].location.city}, ${data[0].location.state}</p>
       </div>
       </div>`
    employeesContainer.innerHTML = html
 }
 
-getEmployees();
+getEmployee();
