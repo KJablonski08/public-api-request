85384c0a2b3dce88662d6c87a1eda24db5c6d4d5
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-08 21:11:54 -0400

    Create js/scripts.js file and wire it up to the HTML

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ baseUrl = 'https://randomuser.me/api/'
 
 fetch(this.baseUrl).then(res => res.json())
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     employees = new Employees();
+//   })
 
+  console.log('connected')
