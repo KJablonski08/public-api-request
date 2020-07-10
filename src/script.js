6b67cce2779a0c4c0d8043ea4c48d655295b8efa
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-09 20:01:18 -0400

    progress in adding eventListener

--- src/script.js
+++ src/script.js
@@ -1,11 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', () => {
-//    employees = new Employees();
-// })
-
-let employees = [];
-let employeesContainer = document.getElementById('gallery')
-
 function getEmployee() {
    baseUrl = 'https://randomuser.me/api/?results=12'
 
@@ -21,30 +14,41 @@ function getEmployee() {
 }
    
 function renderEmployee(data) {
-   let render = ''
-      data.forEach(employee => {
-         let img = employee.picture.large
-         let firstName = employee.name.first
-         let lastName = employee.name.last
-         let email = employee.email
-         let city = employee.location.city
-         let state = employee.location.state
+   employees = []
 
-         render += 
-         `<div class="card">
+   let gallery = document.getElementById('gallery')
+   let galleryObj = gallery.children
+   
+   for (i=0; i < data.length; i++) {
+      const img = data[i].picture.large
+      const firstName = data[i].name.first
+      const lastName = data[i].name.last
+      const email = data[i].email
+      const city = data[i].location.city
+      const state = data[i].location.state
+
+      let html =
+         `<div class="card" id="${i}">
             <div class="card-img-container">
                <img class="card-img" src="${img}" alt="profile picture">
             </div>
             <div class="card-info-container">
                <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
                <p class="card-text">${email}</p>
-               <p class="card-text cap">${data[0].location.city}, ${data[0].location.state}</p>
+               <p class="card-text cap">${city}, ${state}</p>
             </div>
          </div>`
-      })
+      employees.push(html)
+   }
 
-   console.log(render)
-   employeesContainer.innerHTML = render
+   gallery.innerHTML = employees;
+   
+   for (i=0; i< galleryObj.length; i++) {
+      galleryObj[i].addEventListener('click', () => {
+         console.log(galleryObj)
+      })
+   }
+
 }
 
 getEmployee();
