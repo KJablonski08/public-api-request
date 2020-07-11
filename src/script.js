ae1058cc78c9fe8ba0d2fd74dc524b4d3c8b5654
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-07-10 21:47:06 -0400

    refactor event listeners

--- src/script.js
+++ src/script.js
@@ -1,10 +1,4 @@
-
-function nextBtn(obj, i, data) {
-   i += 1 
-   obj = data[i]
-   getModal(obj, i, data)
-   console.log(obj)
- }
+employees = []
 
 function getEmployee() {
    baseUrl = 'https://randomuser.me/api/?results=12&nat=GB,US,NZ,AU,CA,CH,DE,DK,ES,FI,FR,IE,NL'
@@ -26,11 +20,12 @@ function renderEmployee(data) {
    
    for (i=0; i < data.length; i++) {
 
+      employees.push(data[i])
+
       const card = document.createElement('div');
       gallery.appendChild(card);
-      card.className = 'card';
-      card.setAttribute('id', `${i}`)
-      card.onclick = getModal(data[i], i, data)
+      card.className = `card ${i}`;
+      card.onclick = getModal(employees[i], i)
 
       let html =
             `<div class="card-img-container">
@@ -46,92 +41,59 @@ function renderEmployee(data) {
    }
 }
 
-function getModal(obj, i, data) {
+function getModal(obj, index) {
    return function() {
 
-      let modal = document.getElementById('modal')
+      index = index;
+      console.log(index)
 
+      let modal = document.getElementById('modal')
       modal.style.display = ''
 
-      let date = new Date(obj.dob.date)
-      let month = date.getMonth();
-      let day = date.getDate();
-      let year = date.getYear();
-
-      let html = `
-      <div class="modal-container">
-         <div class="modal">
-         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-               <img class="modal-img" src="${obj.picture.large}" alt="profile picture">
-               <h3 id="name" class="modal-name cap">${obj.name.first} ${obj.name.last}</h3>
-               <p class="modal-text">${obj.email}</p>
-               <p class="modal-text cap">${obj.location.city}</p>
-               <hr>
-               <p class="modal-text">${obj.cell}</p>
-               <p class="modal-text">${obj.location.street.number} ${obj.location.street.name}, ${obj.location.city}, ${obj.location.state} ${obj.location.postcode}</p>
-               <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
-            </div>
-         </div>
-
-         <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-         </div>
-      </div>`
-
-      modal.innerHTML = html
-
-      const close = document.getElementById('modal-close-btn');
-      close.addEventListener('click', () => {
-         modal.style.display = 'none';
-      });
-
-      const previous = document.getElementById('modal-prev')
-      let prevCount = 0
-      previous.addEventListener('click', () => {
-         prevCount++
-         console.log(prevCount)
-         if (i > 0 && i < 11) {
-            obj = data[i - prevCount]
-            console.log(obj)
-            
-         //    let newhtml = `
-         //    <div class="modal-container">
-         //       <div class="modal">
-         //       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-         //          <div class="modal-info-container">
-         //             <img class="modal-img" src="${obj.picture.large}" alt="profile picture">
-         //             <h3 id="name" class="modal-name cap">${obj.name.first} ${obj.name.last}</h3>
-         //             <p class="modal-text">${obj.email}</p>
-         //             <p class="modal-text cap">${obj.location.city}</p>
-         //             <hr>
-         //             <p class="modal-text">${obj.cell}</p>
-         //             <p class="modal-text">${obj.location.street.number} ${obj.location.street.name}, ${obj.location.city}, ${obj.location.state} ${obj.location.postcode}</p>
-         //             <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
-         //          </div>
-         //       </div>
-
-         //       <div class="modal-btn-container">
-         //          <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-         //          <button type="button" id="modal-next" class="modal-next btn">Next</button>
-         //       </div>
-         //    </div>`
-
-         // modal.innerHTML = newhtml;
-            // if ((i + prevCount) > 11 ) {
-            //    i = 0
-            // }
-         }
-      })
-
-      const next = document.getElementById('modal-next')
-      next.addEventListener('click', () => {
-         nextBtn(obj, i, data)
-      })
-
+      getHTML(obj)
    };
  }
- 
+
+ function getHTML(obj) {
+   let date = new Date(obj.dob.date)
+   let month = date.getMonth();
+   let day = date.getDate();
+   let year = date.getYear();
+
+   let html = `
+   <div class="modal-container">
+      <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+         <div class="modal-info-container">
+            <img class="modal-img" src="${obj.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${obj.name.first} ${obj.name.last}</h3>
+            <p class="modal-text">${obj.email}</p>
+            <p class="modal-text cap">${obj.location.city}</p>
+            <hr>
+            <p class="modal-text">${obj.cell}</p>
+            <p class="modal-text">${obj.location.street.number} ${obj.location.street.name}, ${obj.location.city}, ${obj.location.state} ${obj.location.postcode}</p>
+            <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
+         </div>
+      </div>
+      <div class="modal-btn-container">
+         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+         <button type="button" id="modal-next" class="modal-next btn">Next</button>
+      </div>
+   </div>`
+
+   modal.innerHTML = html
+ }
+
+modal.addEventListener('click', (e) => {
+
+   if (e.target.className === "modal-close-btn" || e.target.innerHTML === "X") {
+      modal.style.display = 'none';
+   }
+
+   
+});
+  
+
 
 getEmployee();
+console.log(employees)
